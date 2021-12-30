a2c629ebe74c64645820c474ef3793279bbab999
noobcoder-oct <64582595+noobcoder-oct@users.noreply.github.com>
2021-12-30 12:45:04 +0300

    Update кофеварка .cpp

--- кофеварка .cpp
+++ кофеварка .cpp
@@ -4,45 +4,11 @@
 #include
 #define 
 #define 
-int aPin=A0;
-// контакт  подключения вывода реле
-int soundPin=11;
-// частота звукового сигнала
-int freq[3]={587,466,293};
-// переменная для сохранения значения датчика
-int avalue=0;
-// значение уровней
-int levels[3]={600,500,400};
-// текущий уровень
-int level=0;
 
-void setup()
-  {
-  // инициализация последовательного порта
-  Serial.begin(9600);
-  // настройка выводов индикации светодиодов
-  // в режим OUTPUT
-  pinMode(soundPin,OUTPUT);
-  }
 
 void loop()
   {
-  // получение значения с аналогового вывода датчика
-  avalue=analogRead(aPin);
-  // вывод значения в монитор последовательного порта Arduino
-  Serial.print("avalue=");Serial.println(avalue);
-  // вывод звука различной частоты для разных уровней погружения
-  if(avalue>levels[0])
-    tone(soundPin,freq[0],2000);
-  else if(avalue>levels[1])
-    tone(soundPin,freq[1],2000);
-  else if(avalue>levels[2])
-    tone(soundPin,freq[2],2000);
-  else
-    noTone(soundPin);
-  // пауза перед следующим получением значения 1000 мс
-  delay(1000);
-}
+  
 const char 
 const char 
 WiFiServer 
@@ -138,9 +104,9 @@ func main() {
                     
                         webPage +="      <button>PUMP_ON</button>"//создается объект кнопка 
                         webPage +="    </a>&nbsp;"; 
-                        webPage +="  <a href=\"temperature"\">"; 
-                        webPage +=   <
-                        webPage +="  <a href=\"humidity"\">"; 
+                        webPage +="  ;
+                        webPage +=   
+                        webPage +=" <script src=\"https://code.jquery.com/jquery-3.2.1.min.js\"></script>";
                         webPage +="    <a href=\"pump_off\">";
                         webPage +="      <button>PUMP_OFF</button>";
                         webPage +="    </a>";
